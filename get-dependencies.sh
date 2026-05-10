0e84038a768c06924366093df8af8f49bcdf07ee
Link4Electronics <link4electronics@gmail.com>
2026-05-09 23:19:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,6 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    dotnet-runtime-9.0  \
     libdecor            \
     libgdiplus          \
     xmlstarlet          \
@@ -15,6 +14,7 @@ pacman -Syu --noconfirm \
     pipewire-audio      \
     pipewire-jack       \
     sdl2
+#    dotnet-runtime-9.0  \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -24,6 +24,7 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+if [ "$ARCH" = "x86_64" ]; then
 ZIP_LINK=$(wget https://api.github.com/repos/alexjyong/Simitone/releases -O - \
       | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Linux-x64-Release.zip")
 echo "$ZIP_LINK" | awk -F'/' '{tag=$(NF-1); gsub(/^v/, "", tag); print tag; exit}' > ~/version
@@ -37,3 +38,9 @@ bsdtar -xvf /tmp/app.zip -C ./AppDir/bin
 mv -v ./AppDir/bin/Simitone ./AppDir/bin/Simlauncher
 rm -f ./AppDir/bin/simitone.desktop
 rm -f ./AppDir/bin/lib/*.pdb
+else
+mkdir -p ./AppDir/bin/lib/Content/MeshReplace
+bsdtar -xvf Simitone-Linux-arm64-Release.zip -C ./AppDir/bin
+rm -f ./AppDir/bin/simitone.desktop
+rm -f ./AppDir/bin/lib/*.pdb
+fi
