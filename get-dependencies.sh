5546c8cad7f5504105ccb3ddd7bf7301144d8be5
Link4Electronics <link4electronics@gmail.com>
2026-05-09 23:22:09 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -24,22 +24,21 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-if [ "$ARCH" = "x86_64" ]; then
+mkdir -p ./AppDir/bin/lib/Content/MeshReplace
 ZIP_LINK=$(wget https://api.github.com/repos/alexjyong/Simitone/releases -O - \
       | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Linux-x64-Release.zip")
 echo "$ZIP_LINK" | awk -F'/' '{tag=$(NF-1); gsub(/^v/, "", tag); print tag; exit}' > ~/version
+if [ "$ARCH" = "x86_64" ]; then
 if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
 	cat /tmp/download.log
 	exit 1
 fi
 
-mkdir -p ./AppDir/bin/lib/Content/MeshReplace
 bsdtar -xvf /tmp/app.zip -C ./AppDir/bin
 mv -v ./AppDir/bin/Simitone ./AppDir/bin/Simlauncher
 rm -f ./AppDir/bin/simitone.desktop
 rm -f ./AppDir/bin/lib/*.pdb
 else
-mkdir -p ./AppDir/bin/lib/Content/MeshReplace
 bsdtar -xvf Simitone-Linux-arm64-Release.zip -C ./AppDir/bin
 rm -f ./AppDir/bin/simitone.desktop
 rm -f ./AppDir/bin/lib/*.pdb
